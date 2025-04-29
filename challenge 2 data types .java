762ec19c32225325e14b631af72153d11eab886d
Kiragu12 <macharia.kiragu@strathmore.edu>
2025-04-29 13:52:30 +0300

    challenge 2 data types .java

---
+++ challenge 2 data types .java
@@ -0,0 +1,10 @@
+public class DataDemo {
+    public static void main(String[] args) {
+        String courseName = "ICS 2101";
+        int students = 45;
+        
+        System.out.println("Course: " + courseName);
+        System.out.println("Students Enrolled: " + students);
+      
+    }
+}
